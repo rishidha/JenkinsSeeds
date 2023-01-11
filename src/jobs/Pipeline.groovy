4d604229d1386fc54fde45905a30b544b323fc12
rishidha <hrishidhagupta@gmail.com>
2023-01-12 01:00:41 +0530

    Testing

--- src/jobs/Pipeline.groovy
+++ src/jobs/Pipeline.groovy
@@ -1,45 +1,48 @@
-def BuildJob() {
-    job('Build1') {
-        description('This job is to build a docker file and generate image')
-        deliveryPipelineConfiguration('CI1', 'Build1')
-        scm {
-            git {
-                remote {
-                    url('git@github.com:rishidha/hello-world.git')
-                    credentials('github')
-                    branch('*/master')
+ def BuildJob() {
+     deliveryPipelineView("EmailNotificationDSL") {
+         job('Build1') {
+             description('This job is to build a docker file and generate image')
+             deliveryPipelineConfiguration('CI1', 'Build1')
+             scm {
+                 git {
+                     remote {
+                         url('git@github.com:rishidha/hello-world.git')
+                         credentials('github')
+                         branch('*/master')
+                     }
+                 }
+             }
+             steps {
+                 shell('echo "Testing"')
+             }
+             publishers {
+                 downstream("Startup Application1")
+             }
+         }
+     }
+    }
+    BuildJob()
+    def startupjob() {
+        deliveryPipelineView("EmailNotificationDSL") {
+            job('Startup Application1') {
+                description('This job is to build a docker file and generate image')
+                deliveryPipelineConfiguration('CI1', 'Startup Application1')
+                scm {
+                    git {
+                        remote {
+                            url('git@github.com:rishidha/hello-world.git')
+                            credentials('github')
+                            branch('*/master')
+                        }
+                    }
+                }
+                steps {
+                    shell('echo "Testing"')
+                }
+                publishers {
+                    downstream("Startup Application1")
                 }
             }
         }
-        steps {
-            shell('echo "Testing"')
-        }
-        publishers {
-            downstream("Startup Application1")
-        }
     }
-}
-BuildJob()
-def startupjob() {
-
-    job('Startup Application1') {
-        description('This job is to build a docker file and generate image')
-        deliveryPipelineConfiguration('CI1', 'Startup Application1')
-        scm {
-            git {
-                remote {
-                    url('git@github.com:rishidha/hello-world.git')
-                    credentials('github')
-                    branch('*/master')
-                }
-            }
-        }
-        steps {
-            shell('echo "Testing"')
-        }
-        publishers {
-            downstream("Startup Application1")
-        }
-    }
-}
-startupjob()
+    startupjob()
