3fc43d693c5bd6d62a317c1047aa846025103a18
rishidha <hrishidhagupta@gmail.com>
2023-01-12 01:10:56 +0530

    Testing

--- src/jobs/Pipeline.groovy
+++ src/jobs/Pipeline.groovy
@@ -1,5 +1,5 @@
  def BuildJob() {
-     deliveryPipelineView("EmailNotificationDSL") {
+     deliveryPipelineView("Pipeline") {
          job('Build1') {
              description('This job is to build a docker file and generate image')
              deliveryPipelineConfiguration('CI1', 'Build1')
@@ -19,11 +19,22 @@
                  downstream("Startup Application1")
              }
          }
+         pipelineInstances(5)
+         showAggregatedPipeline()
+         columns(2)
+         sorting(Sorting.TITLE)
+         updateInterval(60)
+         enableManualTriggers()
+         showAvatars()
+         showChangeLog()
+         pipelines {
+             component('RishidhaCenter', 'Build1')
+         }
      }
     }
     BuildJob()
     def startupjob() {
-        deliveryPipelineView("EmailNotificationDSL") {
+        deliveryPipelineView("Pipeline") {
             job('Startup Application1') {
                 description('This job is to build a docker file and generate image')
                 deliveryPipelineConfiguration('CI1', 'Startup Application1')
