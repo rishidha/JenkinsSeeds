84d24772defe8cbedc5405241ebf6b0a2d8beda9
rishidha <hrishidhagupta@gmail.com>
2023-01-12 01:28:43 +0530

    Testing

--- src/jobs/Pipeline.groovy
+++ src/jobs/Pipeline.groovy
@@ -1,57 +1,48 @@
- def BuildJob() {
-     deliveryPipelineView("Pipeline") {
-         job('Build1') {
-             description('This job is to build a docker file and generate image')
-             deliveryPipelineConfiguration('CI1', 'Build1')
-             scm {
-                 git {
-                     remote {
-                         url('git@github.com:rishidha/hello-world.git')
-                         credentials('github')
-                         branch('*/master')
-                     }
+ deliveryPipelineView("Pipeline") {
+     job('Build1') {
+         description('This job is to build a docker file and generate image')
+         deliveryPipelineConfiguration('CI1', 'Build1')
+         scm {
+             git {
+                 remote {
+                     url('git@github.com:rishidha/hello-world.git')
+                     credentials('github')
+                     branch('*/master')
                  }
              }
-             steps {
-                 shell('echo "Testing"')
-             }
-             publishers {
-                 downstream("Startup Application1")
-             }
          }
-         pipelineInstances(5)
-         showAggregatedPipeline()
-         columns(2)
-         sorting(Sorting.TITLE)
-         updateInterval(60)
-         enableManualTriggers()
-         showAvatars()
-         showChangeLog()
-         pipelines {
-             component('RishidhaCenter', 'Build1')
+         steps {
+             shell('echo "Testing"')
+         }
+         publishers {
+             downstream("Startup Application1")
          }
      }
-    }
-
-    BuildJob()
-    def startupjob() {
-        deliveryPipelineView("Pipeline") {
-            job('Startup Application1') {
-                description('This job is to build a docker file and generate image')
-                deliveryPipelineConfiguration('CI1', 'Startup Application1')
-                scm {
-                    git {
-                        remote {
-                            url('git@github.com:rishidha/hello-world.git')
-                            credentials('github')
-                            branch('*/master')
-                        }
-                    }
-                }
-                steps {
-                    shell('echo "Testing"')
-                }
-            }
-        }
-    }
-    startupjob()
+     pipelineInstances(5)
+     showAggregatedPipeline()
+     columns(2)
+     sorting(Sorting.TITLE)
+     updateInterval(60)
+     enableManualTriggers()
+     showAvatars()
+     showChangeLog()
+     pipelines {
+         component('RishidhaCenter', 'Build1')
+     }
+     job('Startup Application1') {
+         description('This job is to build a docker file and generate image')
+         deliveryPipelineConfiguration('CI1', 'Startup Application1')
+         scm {
+             git {
+                 remote {
+                     url('git@github.com:rishidha/hello-world.git')
+                     credentials('github')
+                     branch('*/master')
+                 }
+             }
+         }
+         steps {
+             shell('echo "Testing"')
+         }
+     }
+ }
